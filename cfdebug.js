c75b29baa7f57c1cfa849ee408757b2c10037735
YopparaiNeko <kj4cgv@gmail.com>
2013-10-09 15:14:08 -0400

    Created snapshot of current incarnation.

---
+++ cfdebug.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	if($('.cfdebug').length) {
+		var cfDebugBT = $('<div class="cfDebugBT" title="click here for debug">')
+			.css({background:"red", color:"white", position:"fixed", left:"0", bottom:"0", 'z-index':"60000001", padding:"5px", cursor:"pointer", 'user-select': "none"})
+			.html('CF Debug Show')
+			.bind('click',function(){
+				$('.cfDebugOverlay').toggle();
+				$('.cfDebugBT').text('CF Debug '+($('.cfDebugOverlay').is(':visible') ? 'Hide' : 'Show'));
+				$('html').css({overflow: ($('.cfDebugOverlay').is(':visible') ? 'hidden' : 'auto') });
+		});
+		$('table.cfdebug:eq(0)')
+			.wrap(function(i){return $('<div class="cfDebugOverlay">').css({position:"absolute", left:"0", top:"0", right:"0", bottom:"0", 'z-index':60000000, overflow:"auto", background:"white", display:"none", padding:"20px"});})
+			.css({'margin-bottom':"30px"});
+		$('.cfDebugOverlay').before(cfDebugBT);
+	};
+});
