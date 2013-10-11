b7cdf1013cf0d57a14c83d534f138f48362b52d6
Alex Moran <kj4cgv@gmail.com>
2013-10-11 17:10:19 -0400

    Added stop callback to cancel out click.

--- cfdebugJUI.js
+++ cfdebugJUI.js
@@ -12,6 +12,6 @@ $(document).ready(function(){
 			.wrap(function(i){return $('<div id="cfDebugOverlay">').css({position:"absolute", left:"0", top:"0", right:"0", bottom:"0", 'z-index':60000000, overflow:"auto", background:"white", display:"none", padding:"20px"});})
 			.css({'margin-bottom':"30px"});
 		$('#cfDebugOverlay').before(cfDebugBT);
-		$('#cfDebugBT').button().draggable().position({my:"left bottom", at:"left bottom"});
+		$('#cfDebugBT').button().draggable({stop: function(e,ui){setTimeout(function(){$('#cfDebugBT').click();},1)}}).position({my:"left bottom", at:"left bottom"});
 	};
 });
